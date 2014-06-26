97b9e9a092e90c401de4681a1a19bbbc7b8d2746
Jaime Lennox <jaimelennoxster@gmail.com>
2014-06-26 09:23:04 +0100

    Added a fallback to local interface if URL check fails.

--- src/findMyIP/IPUtils.java
+++ src/findMyIP/IPUtils.java
@@ -2,8 +2,10 @@ package findMyIP;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.UnknownHostException;
 
 public class IPUtils {
 
@@ -22,13 +24,47 @@ public class IPUtils {
     }
 
     public static String getIPAddress() {
+        String IPAddress = getIPFromURL();
 
-        try (BufferedReader in = new BufferedReader(new InputStreamReader(
-                IP_CHECK_URL.openStream()));) {
-            return in.readLine();
+        // Fallback.
+        if (IPAddress == null) {
+            IPAddress = getIPFromAPI();
         }
-        catch (Exception e) {
+
+        if (IPAddress == null) {
             return "<unknown>";
         }
+
+        return IPAddress;
+    }
+
+    private static String getIPFromURL() {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(
+                IP_CHECK_URL.openStream()));) {
+            String ipString = in.readLine();
+            if (validIP(ipString)) {
+                return ipString;
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private static String getIPFromAPI() {
+        try {
+            InetAddress IP = InetAddress.getLocalHost();
+            return IP.getHostAddress();
+        }
+        catch (UnknownHostException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static boolean validIP(String IPAddress) {
+        return IPAddress.matches("\\b\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\b");
     }
 }
